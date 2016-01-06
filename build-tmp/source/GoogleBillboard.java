03f94562e99824977d92321303030a427154e1ad
Parker101200 <Parker@cementerprises.com>
2016-01-06 08:17:48 -0800

    found the prime number

--- build-tmp/source/GoogleBillboard.java
+++ build-tmp/source/GoogleBillboard.java
@@ -14,28 +14,34 @@ import java.io.IOException;
 
 public class GoogleBillboard extends PApplet {
 
-public double dNum;
+//public double dNum;
 public final static String e = "2.718281828459045235360287471352662497757247093699959574966967627724076630353547594571382178525166427427466391932003059";  
 public void setup()  
 {            
-	noLoop();  
-	String digits = e.substring(2,12);
-	double dNum = Double.parseDouble(digits);
-	System.out.println(dNum);
+	noLoop();
+    for(int i = 2; i < (e.length()-10); i++)
+    {  
+    	String digits = e.substring(i, i+10);
+    	double num = Double.parseDouble(digits);
+        if(isPrime(num) == true)
+        {
+    	   System.out.println(num);
+        }
+    }
 }  
 public void draw()  
 {   
 
 }  
-public boolean isPrime(double dNum)  
+public boolean isPrime(double num)  
 {   
-    if (dNum < 2)
+    // if (num < 2)
+    // {
+    //     return false;
+    // }
+    for(int k = 2; k <= Math.sqrt(num); k++)
     {
-    	return false;
-    }
-    for(int i = 2; i <= Math.sqrt(dNum); i++)
-    {
-    	if(dNum%i==0)
+    	if(num%k==0)
     	{
     		return false;
     	}
